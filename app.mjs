143867983abb180b49d27f90ffd979e7ecffd4d9
Nausheen Nadeem <nausheennadeem849@gmail.com>
2025-02-21 00:18:04 +0500

    Frontend connect

--- app.mjs
+++ app.mjs
@@ -2,6 +2,10 @@ import express from 'express';
 const app = express()
 const PORT = process.env.PORT || 5001; // 5001 ya koi aur free port
 
+import path from'path';
+const __dirname =path.resolve()
+app.use('/', express.static(path.join(__dirname, './web/dist')))
+
 app.get('/', (req, res) => {
   res.send('Hello NAUSHEEN ok')
 })
